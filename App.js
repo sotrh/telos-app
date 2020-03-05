cdc3fcd64e72de070379e0515445d7e28dd77973
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-03-05 15:09:04 -0700

    Added list

--- App.js
+++ App.js
@@ -1,19 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false,
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  renderSeparator = () => {
+    return (
+      <View 
+        style={{
+          height: 1,
+          width: '100%',
+          backgroundColor: '#CED0CE',
+        }}
+      />
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.listItem}>
+        <FlatList
+          style={styles.list}
+          data={this.state.data}
+          keyExtractor={item => item.email}
+          renderItem={({item}) => (
+            <View style={styles.listItem}>
+              <Text>{item.name.first} {item.name.last}</Text>
+              <Text>{item.email}</Text>
+            </View>
+          )}
+          ItemSeparatorComponent={this.renderSeparator}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  list: {
+    width: '100%',
+  },
+
+  listItem: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  }
 });
